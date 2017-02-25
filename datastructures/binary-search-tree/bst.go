8bb4a3f0a656c0db6316e86a2f739cd9943b554a
Juto Yu <jutkko@gmail.com>
2017-02-25 15:12:13 +0000

    Implement insert, simple print, min and exists

--- datastructures/binary-search-tree/bst.go
+++ datastructures/binary-search-tree/bst.go
@@ -1,30 +1,56 @@
 package bst
 
+import "fmt"
+
 type Node struct {
 	Left  *Node
 	Right *Node
 	Elem  int
 }
 
-func (n *Node) insert(newNode *Node) {
-	if n.Elem == newNode.Elem {
-		return
+func (n *Node) insert(newNode *Node) *Node {
+	if n == nil {
+		return newNode
 	}
 
 	if n.Elem < newNode.Elem {
-		if n.Right == nil {
-			n.Right = newNode
-			return
-		} else {
-			n.Right.insert(newNode)
-		}
+		n.Right = n.Right.insert(newNode)
 	} else {
-		if n.Left == nil {
-			n.Left = newNode
-			return
-		} else {
-			n.Left.insert(newNode)
-		}
+		n.Left = n.Left.insert(newNode)
+	}
+
+	return n
+}
+
+func (n *Node) string() string {
+	if n == nil {
+		return ""
+	}
+
+	return n.Left.string() + fmt.Sprintf(" %d", n.Elem) + n.Right.string()
+}
+
+func (n *Node) min() *Node {
+	if n == nil {
+		return nil
+	}
+
+	if n.Left == nil {
+		return n
+	}
+
+	return n.Left.min()
+}
+
+func (n *Node) exists(key int) bool {
+	if n.Elem == key {
+		return true
+	}
+
+	if n.Elem < key {
+		return n.Right.exists(key)
+	} else {
+		return n.Left.exists(key)
 	}
 }
 
@@ -33,11 +59,21 @@ type Bst struct {
 }
 
 func (b *Bst) Insert(value int) {
+	b.Root = b.Root.insert(&Node{Elem: value})
+}
+
+func (b *Bst) String() string {
+	return b.Root.string()
+}
+
+func (b *Bst) Min() *Node {
+	return b.Root.min()
+}
+
+func (b *Bst) Exists(key int) bool {
 	if b.Root == nil {
-		b.Root = &Node{Elem: value}
-	} else {
-		b.Root.insert(&Node{Elem: value})
+		return false
 	}
 
-	return
+	return b.Root.exists(key)
 }
--- datastructures/binary-search-tree/bst_test.go
+++ datastructures/binary-search-tree/bst_test.go
@@ -4,12 +4,55 @@ import "testing"
 
 func TestInsert(t *testing.T) {
 	bst := Bst{}
+	bst.Insert(9)
+	bst.Insert(0)
+	bst.Insert(5)
 	bst.Insert(5)
 	bst.Insert(5)
 	bst.Insert(1)
 	bst.Insert(4)
-	bst.Insert(9)
 
-	node := &Node{}
-	node.Right.insert(&Node{Elem: 4})
+	if bst.String() != " 0 1 4 5 5 5 9" {
+		t.Fatalf("Insert is not right")
+	}
+}
+
+func TestMinNoMin(t *testing.T) {
+	bst := Bst{}
+
+	if bst.Min() != nil {
+		t.Fatalf("Min is wrong")
+	}
+}
+
+func TestMin(t *testing.T) {
+	bst := Bst{}
+	bst.Insert(9)
+	bst.Insert(0)
+	bst.Insert(5)
+	bst.Insert(5)
+	bst.Insert(5)
+	bst.Insert(-3)
+	bst.Insert(1)
+	bst.Insert(4)
+
+	if bst.Min().Elem != -3 {
+		t.Fatalf("Min is not right: %d", bst.Min())
+	}
+}
+
+func TestExists(t *testing.T) {
+	bst := Bst{}
+	bst.Insert(9)
+	bst.Insert(0)
+	bst.Insert(5)
+	bst.Insert(5)
+	bst.Insert(5)
+	bst.Insert(-3)
+	bst.Insert(1)
+	bst.Insert(4)
+
+	if !bst.Exists(0) {
+		t.Fatalf("Find is not right")
+	}
 }
