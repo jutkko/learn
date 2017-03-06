e13bf31ac0318a7b3eb08efd51cbb2aeb3267592
Juto Yu <jutkko@gmail.com>
2017-03-06 21:19:55 +0000

    More arithmetics

---
+++ python/arithmetic-operations.py
@@ -0,0 +1,16 @@
+def sum(a, b):
+    return a+b
+
+def diff(a, b):
+    return a-b
+
+def product(a, b):
+    return a*b
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+
+    print(sum(a, b))
+    print(diff(a, b))
+    print(product(a, b))
