4723836bc89583d2c5018752ca702ac8615325b1
Juto Yu <jutkko@gmail.com>
2017-03-11 16:03:41 +0000

    Python3 does not allow multiple use of variables in list comprehension
    - x = 3; y = 3; [x+y for x in range(x) for y in range(y)] is not
      allowed!

---
+++ python/list-comprehension.py
@@ -0,0 +1,12 @@
+import unittest
+
+def cuboid(x, y, z, n):
+    print(x, y, z, n)
+    x += 1
+    y += 1
+    z += 1
+    return [[x1, y1, z1] for x1 in range(x) for y1 in range(y) for z1 in range(z) if x1+y1+z1 != n]
+
+class TestTuplify(unittest.TestCase):
+    def testCuboid(self):
+        self.assertEqual(cuboid(1, 1, 1, 2), [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]])
