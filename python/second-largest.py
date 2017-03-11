2098ff39302f8e64756f31ae110b3f4f8fe16c44
Juto Yu <jutkko@gmail.com>
2017-03-11 18:06:40 +0000

    This is the better solution

--- python/second-largest.py
+++ python/second-largest.py
@@ -1,30 +1,21 @@
 import unittest
 
 def find_second_largest(list):
-    max = list[0]
-    for x in list:
-        if x > max:
-            max = x
-
-    not_max = None
-    for x in list:
-        if x != max:
-            not_max = x
-            break
-
-    if not_max == None:
-        return None
-
     result = None
+    max = -101
     for x in list:
-        if x >= not_max and x != max:
-            result = x
+        if max < x:
+            result = max
+            max = x
+        else:
+            if result < x and x != max:
+                result = x
     return result
 
 class TestSecondLargest(unittest.TestCase):
     def test1(self):
         self.assertEqual(find_second_largest([1, 2, 3, 5, 4]), 4)
     def test2(self):
-        self.assertEqual(find_second_largest([5, 5, 5, 5, 5]), None)
+        self.assertEqual(find_second_largest([5, 5, 5, 5, 5]), -101)
     def test3(self):
         self.assertEqual(find_second_largest([5, 5, 5, -5, 5]), -5)
