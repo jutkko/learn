61632bf9b41c691ce0512f73a2cc633fe131b868
Juto Yu <jutkko@gmail.com>
2017-05-01 16:26:19 +0100

    Go concurrency examples from programming in go

---
+++ go-concurrency/primitive-channel.go
@@ -0,0 +1,29 @@
+package main
+
+func createCounter(start int) chan int {
+	next := make(chan int)
+
+	go func(i int) {
+		for {
+			next <- i
+			i++
+		}
+	}(start)
+
+	return next
+}
+
+func main() {
+	// count := createCounter(0)
+
+	// fmt.Printf("Count is %d\n", <-count)
+	// fmt.Printf("Count is %d\n", <-count)
+	// fmt.Printf("Count is %d\n", <-count)
+	// fmt.Printf("Count is %d\n", <-count)
+	// fmt.Printf("Count is %d\n", <-count)
+	// fmt.Printf("Count is %d\n", <-count)
+
+	// close(count)
+
+	rollDice()
+}
